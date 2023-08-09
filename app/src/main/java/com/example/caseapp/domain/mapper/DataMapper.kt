16ae2eba96c9f736bcbe8474aba915ab39044cc2
alicansekban <alicansekban@hotmail.com>
2023-08-09 23:18:09 +0300

    non used fun deleted

--- app/src/main/java/com/example/caseapp/domain/mapper/DataMapper.kt
+++ app/src/main/java/com/example/caseapp/domain/mapper/DataMapper.kt
@@ -4,7 +4,6 @@ import com.example.caseapp.base.ArticlesItem
 import com.example.caseapp.data.local.dbModel.ArticleEntity
 import com.example.caseapp.domain.model.ArticleUIModel
 import com.example.caseapp.utils.toDate
-import java.util.Date
 import javax.inject.Inject
 
 class DataMapper @Inject constructor() {
@@ -32,18 +31,5 @@ class DataMapper @Inject constructor() {
             content = item.content,
             id = item.id
         )
-    }
-
-    fun mapListToUIModel(items: List<ArticleEntity>): List<ArticleUIModel> {
-        return items.map { item ->
-            ArticleUIModel(
-                publishedAt = item.publishedAt,
-                urlToImage = item.urlToImage,
-                description = item.description,
-                title = item.title,
-                content = item.content,
-                id = item.id
-            )
-        }
     }
 }
